15383eef30d3343f8d8fc438757a605ba33f59b7
John Kotz <john.lyme@mac.com>
2019-04-30 22:48:14 -0400

    Converting set_merge to be set_intersect

--- set_intersect.c
+++ set_intersect.c
@@ -1,7 +1,7 @@
 /* 
- * set_iterate2.c - test program for simple set module
+ * set_intersect.c - test program for simple set module
  *
- * David Kotz - April 2016, updated May 2016, 2017, 2019
+ * John Kotz - April 2019, Adapted from set_iterate
  */
 
 #include <stdio.h>
@@ -10,11 +10,17 @@
 #include "set.h"
 #include "memory.h"
 
-static void set_merge(set_t *setA, set_t *setB);
-static void set_merge_helper(void *arg, const char *key, void *item);
+static set_t *set_intersect(set_t *setA, set_t *setB);
+static void set_intersect_helper(void *arg, const char *key, void *item);
 static void itemprint(FILE *fp, const char *key, void *item);
 static void itemdelete(void *item);
 static int *intsave(int item);
+
+typedef struct set_intersect_data {
+  set_t *setA;
+  set_t *setB;
+  set_t *destination;
+} set_intersect_data_t;
 
 int main() 
 {
@@ -22,11 +28,6 @@ int main()
 
   setA = assertp(set_new(), "setA");
   setB = assertp(set_new(), "setB");
-  result = assertp(set_new(), "result");
-
-  printf("Empty result set: ");
-  set_print(result, stdout, itemprint);
-  putchar('\n');
   
   printf("Building set A: ");
   set_insert(setA, "Brown", intsave(10));
@@ -48,13 +49,8 @@ int main()
   set_print(setB, stdout, itemprint);
   putchar('\n');
   
-  printf("\nMerge of setA into result: \n");
-  set_merge(result, setA);
-  set_print(result, stdout, itemprint);
-  putchar('\n');
-
-  printf("\nMerge of setB into result: \n");
-  set_merge(result, setB);
+  printf("\nIntersect setA and setB into result: \n");
+  result = set_intersect(setB, setA);
   set_print(result, stdout, itemprint);
   putchar('\n');
 
@@ -67,33 +63,34 @@ int main()
 /* Merge the second set into the first set;
  * the second set is unchanged.
  */
-static void 
-set_merge(set_t *setA, set_t *setB)
+static set_t *set_intersect(set_t *setA, set_t *setB)
 {
-  set_iterate(setB, setA, set_merge_helper);
+  set_t *set = set_new();
+  set_intersect_data_t *intersect_data = assertp(malloc(sizeof(set_intersect_data_t)), "intersect_data");
+  
+  intersect_data->setA = setA;
+  intersect_data->setB = setB;
+  intersect_data->destination = set;
+
+  set_iterate(setA, intersect_data, set_intersect_helper);
+  set_iterate(setB, intersect_data, set_intersect_helper);
+  
+  return set;
 }
 
-/* Consider one item for insertion into the other set.
- * If the other set does not contain the item, insert it;
- * otherwise, update the other set's item with sum of item values.
+/* Help the set_intersect function to complete it's opperation
  */
-static void 
-set_merge_helper(void *arg, const char *key, void *item)
+static void set_intersect_helper(void *arg, const char *key, void *item)
 {
-  set_t *setA = arg;
-  int *itemB = item;
-  
-  // find the same key in setA
+  set_intersect_data_t *intersect_data = arg;
+  set_t *setA = intersect_data->setA;
+  set_t *setB = intersect_data->setB;
+  set_t *destination = intersect_data->destination;
+
   int *itemA = set_find(setA, key);
-  if (itemA == NULL) {
-    // not found: insert it
-    set_insert(setA, key, intsave(*itemB));
-    printf("\t%s added\n", key);
-  } else {
-    // add to the existing value
-    *itemA += *itemB;
-    printf("\t%s exists\n", key);
-  }
+  int *itemB = set_find(setB, key);
+  if (itemA != NULL && itemB != NULL)
+    set_insert(destination, key, intsave(*itemB));
 }
 
 /* print the given item to the given file.
